0632b9053df68c92924378c834fab0ccce95dbf7
Lucy Jones <lucyjpjones@gmail.com>
2021-03-05 17:10:53 +0000

    moved cards and removed interval timer

--- src/components/ExperienceSection.js
+++ src/components/ExperienceSection.js
@@ -16,21 +16,8 @@ function ExperienceSection() {
         <Container id="edu-container">
             <h3 className='underline--text'>Qualifications</h3>
             <Row>
-                <Carousel controls={false} style={{width:'90%'}} >
-                    <Carousel.Item>
-                    <Card>
-                        <Card.Img variant="top" src={process.env.PUBLIC_URL + '/images/university.png'} />
-                        <Card.Body>
-                        <Card.Title style={{fontSize:'1rem'}}>BSc SPORT AND EXERCISE SCIENCE</Card.Title>
-                        <Card.Text>
-                            <p style={{fontWeight:'600', color:'#394867', fontSize:'.9rem'}}>First Class Honors</p>
-                            <p style={{fontSize:'.8rem'}}>Dissertation: Investigating whether single and multijoint isometric strength assessments can be valid
-                            predictors of dynamic performance.</p>
-                        </Card.Text>
-                        </Card.Body>
-                    </Card>
-                    </Carousel.Item>
-                    <Carousel.Item>
+                <Carousel controls={false} interval={null} style={{width:'90%'}} >
+                <Carousel.Item>
                     <Card>
                         <Card.Img variant="top" src={process.env.PUBLIC_URL + '/images/st-marys.png'} />
                         <Card.Body>
@@ -40,6 +27,19 @@ function ExperienceSection() {
                         <p style={{fontSize:'.8rem'}}>Dissertation: The influence of physical load and
                         contextual variables on measures of post-match
                         fatigue in elite-level soccer players</p>
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                    <Card>
+                        <Card.Img variant="top" src={process.env.PUBLIC_URL + '/images/university.png'} />
+                        <Card.Body>
+                        <Card.Title style={{fontSize:'1rem'}}>BSc SPORT AND EXERCISE SCIENCE</Card.Title>
+                        <Card.Text>
+                            <p style={{fontWeight:'600', color:'#394867', fontSize:'.9rem'}}>First Class Honors</p>
+                            <p style={{fontSize:'.8rem'}}>Dissertation: Investigating whether single and multijoint isometric strength assessments can be valid
+                            predictors of dynamic performance.</p>
                         </Card.Text>
                         </Card.Body>
                     </Card>
