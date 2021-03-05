9b97491492f6004e7c4dbd5ee3e540acf1369c13
Lucy Jones <lucyjpjones@gmail.com>
2021-03-05 14:53:50 +0000

    img changed, added height to container section

--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -9,18 +9,21 @@ function HomeSection() {
     const textStrings = ['Performance Coach', 'Crystal Palace FC']
 
     return (
-        <div className="main-image">
-            <h1>Michael Eglon</h1>
-            <ITyped className='typing-text'
-                showCursor={false}
-                strings={textStrings}
-                typeSpeed={150}
-                backSpeed={100}
-                startDelay={100}
-                backDelay={150}
-            />
-            <a target="_blank" rel="noreferrer" href={process.env.PUBLIC_URL + '/files/mjbe-resume.pdf'} className='btn-cv'>Get CV</a>
-        </div>
+            <div className="main-image">
+                <h1>Michael Eglon</h1>
+                <ITyped className='typing-text'
+                    showCursor={false}
+                    strings={textStrings}
+                    typeSpeed={150}
+                    backSpeed={100}
+                    startDelay={100}
+                    backDelay={150}
+                />
+                <a target="_blank" rel="noreferrer" href={process.env.PUBLIC_URL + '/files/mjbe-resume.pdf'} className='btn-cv'>Get CV</a>
+                <a href='#about' className='arrow'>
+                 <i className="fas fa-angle-down fa-3x bounce" style={{marginTop:'50px', color:'#fafafa'}}></i>
+                </a>
+            </div>
     );
 }
 
