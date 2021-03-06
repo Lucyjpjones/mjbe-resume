ea8676bf9094cf8e3cf0f4eeaa5285bbff4c8d32
Lucy Jones <lucyjpjones@gmail.com>
2021-03-06 17:07:02 +0000

    changed social icon padding

--- src/components/QualificationSection.js
+++ src/components/QualificationSection.js
@@ -122,62 +122,6 @@ function QualificationSection() {
                     </MDBRow>
                     </MDBCarouselInner>
                 </MDBCarousel>
-                {/* <Carousel controls={false} interval={null} style={{width:'90%'}} >
-                <Carousel.Item>
-                    <Card>
-                        <Card.Img variant="top" src={process.env.PUBLIC_URL + '/images/st-marys.png'} />
-                        <Card.Body>
-                        <Card.Title style={{fontSize:'1rem'}}>MSc Strength & Conditioning</Card.Title>
-                        <Card.Text>
-                        <p style={{fontWeight:'600', color:'#394867', fontSize:'.9rem'}}>Distinction</p>
-                        <p style={{fontSize:'.8rem'}}>Dissertation: The influence of physical load and
-                        contextual variables on measures of post-match
-                        fatigue in elite-level soccer players</p>
-                        </Card.Text>
-                        </Card.Body>
-                    </Card>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                    <Card>
-                        <Card.Img variant="top" src={process.env.PUBLIC_URL + '/images/university.png'} />
-                        <Card.Body>
-                        <Card.Title style={{fontSize:'1rem'}}>BSc Sport and Exercise Science</Card.Title>
-                        <Card.Text>
-                            <p style={{fontWeight:'600', color:'#394867', fontSize:'.9rem'}}>First Class Honors</p>
-                            <p style={{fontSize:'.8rem'}}>Dissertation: Investigating whether single and multijoint isometric strength assessments can be valid
-                            predictors of dynamic performance.</p>
-                        </Card.Text>
-                        </Card.Body>
-                    </Card>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                    <Card>
-                        <Card.Img variant="top" src={process.env.PUBLIC_URL + '/images/award.png'} />
-                        <Card.Body>
-                        <Card.Title style={{fontSize:'1rem'}} >Addional Qualifications</Card.Title>
-                        <Card.Text style={{fontSize:'.8rem'}}>
-                            <ul className='ul-experience' >
-                                <li>
-                                    <p style={{fontWeight: 'bold'}}>UKSCA <span style={{color:'#9ba4b4', fontSize: '80%'}}>3 out of 4 parts completed</span></p>
-                                </li>
-                                <li>
-                                    <p style={{fontWeight: 'bold'}}>ALTIS <span style={{color:'#9ba4b4', fontSize: '80%'}}> Completing the “Need for Speed” course</span></p>
-                                </li>
-                                <li>
-                                    <p style={{fontWeight: 'bold'}}>BRITISH WEIGHTLIFTING <span style={{color:'#9ba4b4', fontSize: '80%'}}> Level 1 Assistant Weightlifting Coach certification</span></p>    
-                                </li>
-                                <li>
-                                    <p style={{fontWeight: 'bold'}}>PERSONAL TRAINING <span style={{color:'#9ba4b4', fontSize: '80%'}}> Active IQ Level 3 Diploma in Fitness, Instructing and Personal Training</span></p>   
-                                </li>
-                                <li>
-                                    <p style={{fontWeight: 'bold'}}>FA <span style={{color:'#9ba4b4', fontSize: '80%'}}> Futsal Level 1, FA Coaching Level 1, First Aid, Safeguarding Children</span></p>
-                                </li>
-                            </ul>
-                        </Card.Text>
-                        </Card.Body>
-                    </Card>
-                    </Carousel.Item>
-                </Carousel> */}
         </MDBContainer>
     );
 }
