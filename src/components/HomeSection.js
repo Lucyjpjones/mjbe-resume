16f44dec40ac5dea48a34aad7a030715bff44cc6
Lucy Jones <lucyjpjones@gmail.com>
2021-03-10 09:20:07 +0000

    added cv download icon on hover

--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ITyped from 'react-ityped';
+import { IoIosArrowDown } from "react-icons/io";
+import { GrDownload } from "react-icons/gr";
 import '../App.css'
 import './Home.css'
 
 function HomeSection() {
 
     const textStrings = ['Performance Coach', 'Crystal Palace FC']
+
+    const [innerText, setInnerText] = useState(false);
+
 
     return (
             <div className="main-image">
@@ -18,9 +23,17 @@ function HomeSection() {
                     startDelay={100}
                     backDelay={150}
                 />
-                <a target="_blank" rel="noreferrer" href={process.env.PUBLIC_URL + '/files/mjbe-resume.pdf'} className='btn-cv'>Get CV</a>
+                <a 
+                    target="_blank" 
+                    rel="noreferrer" 
+                    href={process.env.PUBLIC_URL + '/files/mjbe-resume.pdf'} 
+                    className='btn-cv' 
+                    onMouseEnter={() => setInnerText(true)}
+                    onMouseLeave={() => setInnerText(false)} >
+                        {innerText ? <GrDownload /> : <div>Get CV</div>}
+                </a>
                 <a href='#about' className='arrow'>
-                 <i className="fas fa-angle-down fa-3x bounce"></i>
+                 <IoIosArrowDown className="arrow-icon fa-3x bounce" />
                 </a>
             </div>
     );
